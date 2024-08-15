d63b674840554d1df51c52b80d41385cac81aa81
KhaNguyen0611 <khamarcel@gmail.com>
2024-08-15 09:21:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
 //Tong 2 so
-function sum(numberOne , numberTwo){
+function sum(numberOne , numberTwo) {
     var result = numberOne + numberTwo
     return result;
 }
 //Tong mang
 function sumArray(array) {
-    sum = 0;
+    var value = 0;
     for(let i = 0; i < array.length; i++) {
-        sum = sum + array[i];
+        value = value + array[i];
     }
-    return sum;
+    return value;
 }
 //Tim min, max array
 function findMinMax(array) {
     let min = array[0];
     let max = array[0];
-    for(let i=0; i<array.length; i++) {
-        if(array[i]<min) {
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] < min) {
             min = array[i];
         }
     }
-    for(let i=0; i<array.length; i++) {
-        if(array[i]>max) {
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] > max) {
             max = array[i];
         }
     }
-    return {min,max};
+    return {min, max};
 }
-console.log(sum);
 //Dem so ky tu trong chuoi
 const text = "Hello1"
-function demChuoi(string) {
+function countString(string) {
    const result = string.length;
    return result;
 }
