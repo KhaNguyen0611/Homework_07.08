b6c40bf4ff63b6233ce4c4962b2f424600ccdde1
KhaNguyen0611 <khamarcel@gmail.com>
2024-08-27 09:56:27 +0700

    Update script.js
    Update Function:
    - Function sumTwoDigits:
      + Check input is number (Yes: sum; No: return 0)
    - Function sumDigitsInArray:
      + Check input is array and value in array is number (Yes: sum digits in array; No: return 0)
      + Format code flow extension Prettier.
      + Unified function data type.
      + Change name: value to total
      + Change name function: sumArray to  sumDigitsInArray.
    - Function findMinMaxInArray:
      + Change name function: findMinMaxArray to findMinMaxInArray
      + Format code flow extension Prettier.
      + Unified function data type.
    - Function countNumberCharacterInString
      + Change name Function: countStringArray to countNumberCharacterInString.
      + Check input is string (Yes: countNumberCharacterInString; No: return 0).

--- script.js
+++ script.js
@@ -1,40 +1,49 @@
 //Sum two digits
-function sumTwoDigits(numberOne , numberTwo) {
-    return numberOne + numberTwo;
+function sumTwoDigits(numberOne, numberTwo) {
+  if (typeof numberOne !== "number" || typeof numberTwo !== "number") {
+    return 0;
+  }
+  return numberOne + numberTwo;
 }
 //Sum digits in array
-function sumArray(array) {
-    let value = 0;
-    for (let i = 0; i < array.length; i++) {
-        if (typeof(array[i]) === 'number') {
-        value += array[i];
-        }
-        else {
-            return 'noSum';
-        }
+function sumDigitsInArray(arr) {
+  if (!Array.isArray(arr)) {
+    return 0;
+  }
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== "number" || !Array.isArray(arr)) {
+      return 0;
+    } else {
+      total += arr[i];
     }
-    return value;
+  }
+  return total;
 }
 //Find min, max in array
-function findMinMaxArray(array) {
-    let min = array[0];
-    let max = array[0];
-    for (let i = 0; i < array.length; i++) {
-        if (typeof(array[i]) === 'number') {
-            if (array[i] < min) {
-                min = array[i];
-            }
-            else if (array[i] > max) {
-                max = array[i];
-            }
-        else {
-            return 'notFind';
-        }
-        }
+function findMinMaxInArray(array) {
+  if (!Array.isArray(array)) {
+    return [];
+  }
+  let min = array[0];
+  let max = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === "number") {
+      if (array[i] < min) {
+        min = array[i];
+      } else if (array[i] > max) {
+        max = array[i];
+      }
+    } else {
+      return [];
     }
-    return [min, max];
+  }
+  return [min, max];
 }
 //Count string in array
-function countStringArray(string) {
-    return string.length;
+function countNumberCharacterInString(string) {
+  if (typeof string !== "string") {
+    return 0;
+  }
+  return string.length;
 }
